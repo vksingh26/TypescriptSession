ac49a7b0533cf0d3412bc04dce7dd3fefdb7ef01
vksingh26 <vksingh20@outlook.com>
2019-02-25 16:33:14 +0530

    Static properties and methods concept.

--- main/src/scripts/getterSetter.ts
+++ main/src/scripts/getterSetter.ts
@@ -9,7 +9,7 @@ class Forest{
 
     set treeName(value:string) {
         if (value.length > 5) {
-            this._treeName = 'Big Banyan Tree';
+            this._treeName = value;
         } else {
             this._treeName = 'Neem Tree'
         }
@@ -22,3 +22,19 @@ forest.treeName = 'Neem';
 console.log(forest.treeName);
 forest.treeName = 'Bigger Tree';
 console.log(forest.treeName);
+
+
+//static properties and methods
+
+class StaticItem {
+    static itmeOne: number = 20;
+    //if we want to access this itemone property outside the class we need to instantiate it first,
+    //so to access this without instantiating we can make it static and same for function too.
+
+    static calculator(addItemTwo: number): number {
+        return this.itmeOne + addItemTwo;
+    }
+}
+
+console.log(StaticItem.itmeOne); //as we can see here we are accessing property and methods 
+console.log(StaticItem.calculator(5)); // of a class without instantiating it
